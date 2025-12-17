6d62fe0b6bee0b4498762ed124791a7e77d00d7d
jubayer-bd <mjh84349@gmail.com>
2025-12-17 23:28:03 +0600

    vercel redeploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { RouterProvider } from "react-router";
-import { router } from "./Routes/Routes.jsx";
 import AuthProvider from "./context/AuthProvider.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
+import { router } from "./routes/Routes.jsx";
 
 // Create QueryClient instance
 const queryClient = new QueryClient();
